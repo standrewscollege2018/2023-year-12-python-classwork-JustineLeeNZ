bbb0974731e2db59ca9080281e0bb251c34eae23
JustineLeeNZ <jlu@stac.school.nz>
2023-02-13 21:35:38 +1300

    Example of using a list of lists
    Set up a list of lists to store info about clients (name and age).

---
+++ clients_list_of_lists.py
@@ -0,0 +1,25 @@
+""" manage client info using a list of lists - Justine Lee 13/2/2023 """
+
+# list of clients
+client_list = [["Justine Lee", 21],["John Smith", 45]]
+
+# display each client in the list
+for name in client_list:
+    print(name)
+
+# display each client in a numbered list
+
+
+
+
+# delete the first client from the list
+
+
+
+# modify the age of the second client in the list
+
+
+
+# add a new client to the list - name and age
+
+
